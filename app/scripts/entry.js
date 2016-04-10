327d3c4ce858297717ca026eb44d4c08c8463ad0
Will Cook <wellsee83@gmail.com>
2016-04-10 14:56:45 -0500

    Post success attempts

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -5,14 +5,43 @@ import listView from './views/listView';
 import voteCollection from './collections/voteCollection';
 import voteView from './views/listView';
 import userCollection from './collections/userCollection';
+import displayListCollection from './collections/displayListCollection';
+import singleListView from './views/singleListView';
 
 let compiledLists = new listCollection();
 var settings = {
 	success: function() {
 		compiledLists.forEach((list) => {
-			let newListView = new listView(list.get('image_ref'), list.get('source_ref'), list.get('list_desc'), list.get('list_title'), list.get('id'));
+			let newListView = new listView(list.get('image_ref'), list.get('source_ref'), list.get('list_desc'), list.get('list_title'), list.get('id'), list.get('items'));
 			$('.list_row').append(newListView.el);
 		});
 	}
 };
 compiledLists.fetch(settings);
+
+let largeViews = new displayListCollection();
+var settings = {
+	success: function() {
+		largeViews.forEach((list) => {
+			let newSingleListView = new singleListView(list.get('image_ref'), list.get('source_ref'), list.get('list_desc'), list.get('list_title'), list.get('id'), list.get('items'));
+			console.log(list.id);
+			console.log(list.items);
+			$('.list_page').append(newSingleListView.el);
+		});
+
+	}
+};
+largeViews.fetch(settings);
+
+let newSubmission = new listCollection();
+$('form').submit(function(e) {
+	e.preventDefault();
+	console.log('hi');
+	let submission = {
+        list_winner: $('#list_winner').val(),
+        list_title: $('#list_title').val(),
+        list_desc: $('#list_desc').val(),
+        list_url: $('#list_url').val(),
+        }
+	newSubmission.create(submission);
+});
--- app/scripts/views/listView.js
+++ app/scripts/views/listView.js
@@ -1,6 +1,9 @@
 import Backbone from 'backbone';
 import $ from 'jquery';
 import voteCollection from '../collections/voteCollection';
+import displayListCollection from '../collections/displayListCollection';
+import singleListView from '../views/singleListView';
+import listCollection from '../collections/listCollection';
 
 let userVote = new voteCollection();
 
@@ -8,8 +11,8 @@ const listView = Backbone.View.extend({
 	className: 'voting_container',
 	events: {
         'click .up_vote': 'upVote',
-        'click .down_vote': 'downVote'
-
+        'click .down_vote': 'downVote',
+        // 'click .list_poster': 'showList'
     },
 	initialize: function(image_ref, source_ref, list_desc, list_title, id) {
 		this.image_ref = image_ref;
@@ -29,7 +32,6 @@ const listView = Backbone.View.extend({
 	},
 	render: function() {
 		this.$el.html(this.template());
-		console.log(this.id);
 	},
 	upVote: function() {
 		let newVote = {
@@ -48,7 +50,26 @@ const listView = Backbone.View.extend({
 			list_id: this.id
 		};
 		userVote = $.post('https://wolfpack-lists.herokuapp.com/api/votes', {newVote});
-	}
+	},
+	// showList: function(e) {
+	// 	e.preventDefault();
+	// 	let largeViews = new listCollection();
+	// 	var settings = {
+	// 		success: function() {
+	// 			largeViews.forEach((list) => {
+	// 				let newSingleListView = new singleListView(list.get('image_ref'), list.get('source_ref'), list.get('list_desc'), list.get('list_title'), list.get('id'), list.get('items'));
+	// 				console.log(list.id);
+	// 				console.log(list.items);
+	// 				console.log(newSingleListView);
+	// 				$('.list_page').append(newSingleListView.el);
+	// 				// window.location = 'list_page_default.html';
+	// 			});
+	// 		}
+	// 	};
+	// 	largeViews.fetch(settings);
+		
+	// 	console.log('yo');
+	// }
 });
 
 export default listView;
