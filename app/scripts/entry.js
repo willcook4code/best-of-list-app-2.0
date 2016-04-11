b1673ce89864744335a0eac8fca903983d412d75
Will Cook <wellsee83@gmail.com>
2016-04-11 01:11:09 -0500

    moved submit form back to its file

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -34,60 +34,15 @@ let largeViews = new listCollection();
 
 largeViews.fetch(settings);
 
-// let newUserSubmission = new submitFormCollection();
+// let largeViews = new listCollection();
+//     var settings = {
+//     success: function() {
+//         largeViews.forEach((list) => {
+//             let newSingleListView = new singleListView(list.get('image_ref'), list.get('source_ref'), list.get('list_desc'), list.get('list_title'), list.get('id'));
+//             $('.list_page').append(newSingleListView.el);
+//         });
+//     }
+// };        
 
-// $('.submit_long_btn').on('click', e => {
-// 	let submission = {
+// largeViews.fetch(settings);
 
-// 	    list_winner: $('#list_winner').val(),
-// 		list_title: $('#list_title').val(),
-// 		list_desc: $('#list_desc').val(),
-// 		list_url: $('#list_url').val(),
-// 		one: $('.list_item_1').val(),
-// 		two: $('.list_item_2').val(), 
-// 		three: $('.list_item_3').val(),
-// 		four: $('.list_item_4').val(),
-// 		five: $('.list_item_5').val(),
-// 		six: $('.list_item_6').val(),
-// 		seven: $('.list_item_7').val(), 
-// 		eight: $('.list_item_8').val(),
-// 		nine: $('.list_item_9').val(),
-// 		ten: $('.list_item_10').val()
-// 	};
-
-// 	var postSettings = {
-// 		url: 'https://wolfpack-lists.herokuapp.com/api/lists',
-// 		type: 'post',
-// 		data: submission,
-// 		dataType: 'json',
-// 		success: function(data) {
-// 			console.log('I run good');
-// 		},
-// 		error: function(err) {
-// 			console.log('try again');
-// 		},
-// 		complete: function(){
-// 			console.log('I run');
-// 		}
-// 	};
-
-// 	$.ajax(postSettings);
-// });
-
-
-<<<<<<< HEAD
-});
-
-let largeViews = new listCollection();
-    var settings = {
-    success: function() {
-        largeViews.forEach((list) => {
-            let newSingleListView = new singleListView(list.get('image_ref'), list.get('source_ref'), list.get('list_desc'), list.get('list_title'), list.get('id'));
-            $('.list_page').append(newSingleListView.el);
-        });
-    }
-};        
-
-largeViews.fetch(settings);
-=======
->>>>>>> e9d8a494465525d9c09e6991b547de936a083bf7
