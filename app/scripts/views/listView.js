f261865ab5e250a11567c6c8fecb6647b3d33a35
Janina Hartley <jhartley1412@gmail.com>
2016-04-11 09:09:36 -0500

    styled # of votes

--- app/scripts/views/listView.js
+++ app/scripts/views/listView.js
@@ -34,7 +34,7 @@ const listView = Backbone.View.extend({
 				<div class="up_vote"> <i class="fa fa-arrow-up fa-2x"></i> </div>
 				<div class="down_vote"> <i class="fa fa-arrow-down fa-2x"></i> </div>
 			</div>
-			<div class="aggro">${this.aggregate_votes}</div>
+			<div class="aggro">${this.aggregate_votes} votes </div>
 			
 		`
 	},
