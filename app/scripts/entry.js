67c4b97a96dcb27ca6cba74cd6f27bbb2ea9bee9
Will Cook <wellsee83@gmail.com>
2016-06-07 11:16:31 -0500

    Deleted commented out code

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,9 +1,7 @@
 import '../styles/main.scss';
 import $ from 'jquery';
 import listCollection from './collections/listCollection';
-// import listView from './views/listView';
 import voteCollection from './collections/voteCollection';
-// import voteView from './views/listView';
 import userCollection from './collections/userCollection';
 import displayListCollection from './collections/displayListCollection';
 import Lists from './components/Lists.js';
@@ -18,40 +16,6 @@ $.ajaxSetup({
 
 ReactDOM.render(<Lists/>, document.querySelector(".lists_container"));
 
-// let newUserSubmission = new voteCollection();
-
-// let topLists = new listCollection();
-// var settings = {
-// 	success: function() {
-// 		topLists.forEach((list) => {
-// 			let newListView = new listView(list.get('image_ref'), list.get('source_ref'), list.get('list_desc'), list.get('list_title'), list.get('aggregate_votes'), list.get('id'));
-// 			$('.most_rated').append(newListView.el);
-// 		});
-// 	}
-// };
-// topLists.fetch(settings);
-
-// let worstLists = new displayListCollection();
-// var settings = {
-// 	success: function() {
-// 		worstLists.forEach((list) => {
-// 			let newListView = new listView(list.get('image_ref'), list.get('source_ref'), list.get('list_desc'), list.get('list_title'), list.get('aggregate_votes'), list.get('id'));
-// 			$('.least_rated').append(newListView.el);
-// 		});
-// 	}
-// };
-// worstLists.fetch(settings);
-
-// let newestLists = new userCollection();
-// var settings = {
-// 	success: function() {
-// 		newestLists.forEach((list) => {
-// 			let newListView = new listView(list.get('image_ref'), list.get('source_ref'), list.get('list_desc'), list.get('list_title'), list.get('aggregate_votes'), list.get('id'));
-// 			$('.user_added').append(newListView.el);
-// 		});
-// 	}
-// };
-// newestLists.fetch(settings);
 // JANINA'S CODE
 $(document).ready(function(){
 		$('.most_rated_tab').click(function(){
